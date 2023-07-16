c4c71665193d91c2aaad8424329638e52f6e2c1f
Xiaohui Xu <35696497+shefferxu@users.noreply.github.com>
2023-07-16 11:43:12 +0800

    register update

--- SQLiteCon.R
+++ SQLiteCon.R
@@ -2,6 +2,9 @@ library(RSQLite)
 mypath<-dirname(rstudioapi::getActiveDocumentContext()$path)
 setwd(mypath)
 
+# 读取RSQLite中的数据 -----------------------------------------------------------
+
+
 # 连接到特定位置的数据库文件
 con <- dbConnect(SQLite(), dbname = "credentials.sqlite")
 
@@ -18,3 +21,25 @@ print(result)
 dbWriteTable(con, "your_table_name", new_rows, append = TRUE)
 # 关闭连接
 dbDisconnect(con)
+
+
+# 随机生成卡密储存至Rdata中------------------------------------------------------------------
+
+# 创建包含大小写英文字母的字符向量
+letters <- c(letters, LETTERS)
+
+# 创建一个空的数据框
+random_strings_df <- data.frame(strings = character(), stringsAsFactors = FALSE)
+
+# 生成100个随机字符串并放入数据框
+for (i in 1:300) {
+  random_letters <- sample(letters, 20)
+  random_string <- paste(random_letters, collapse = "")
+  random_strings_df <- rbind(random_strings_df, data.frame(strings = random_string, stringsAsFactors = FALSE))
+}
+# random_strings_df$strings <- paste0("day",random_strings_df$strings)
+random_strings_df$day <- 3
+random_strings_df[200:300,2] <- 7
+keydatabase <- random_strings_df
+colnames(keydatabase)[1] <- "key"
+save(keydatabase, file = "./data/key.RData")
--- global.R
+++ global.R
@@ -5,34 +5,22 @@ library(shiny)
 library(shinymanager)
 library(shinythemes)
 # you can use keyring package to set database key
-# library(keyring)
-# key_set("xxh")
-# 创建包含大小写英文字母的字符向量
-letters <- c(letters, LETTERS)
+library(keyring)
+# passphrase <- key_set("R-shinymanager-key", "55mayday")
 
-# 创建一个空的数据框
-random_strings_df <- data.frame(strings = character(), stringsAsFactors = FALSE)
-
-# 生成100个随机字符串并放入数据框
-for (i in 1:2) {
-  random_letters <- sample(letters, 6)
-  random_string <- paste(random_letters, collapse = "")
-  random_strings_df <- rbind(random_strings_df, data.frame(strings = random_string, stringsAsFactors = FALSE))
-}
-random_strings_df$strings <- paste0("day",random_strings_df$strings)
 # 
 # 
-# 
-dailydf <- data.frame(
-  user =random_strings_df$strings, # mandatory
-  password =random_strings_df$strings, # mandatory
-  start = c("2019-04-15"), # optinal (all others)
-  expire = c("2029-05-15"),
-  admin = c(FALSE),
-  # comment = "Simple and secure authentification mechanism
-  # for single ‘Shiny’ applications.",
-  stringsAsFactors = FALSE
-)
+# new random user
+# dailydf <- data.frame(
+#   user =random_strings_df$strings, # mandatory
+#   password =random_strings_df$strings, # mandatory
+#   start = c("2019-04-15"), # optinal (all others)
+#   expire = c("2029-05-15"),
+#   admin = c(FALSE),
+#   # comment = "Simple and secure authentification mechanism
+#   # for single ‘Shiny’ applications.",
+#   stringsAsFactors = FALSE
+# )
 
 # define some credentials
 credentials <- data.frame(
@@ -49,11 +37,11 @@ credentials <- data.frame(
 credentials <- rbind(dailydf,credentials)
 
 # Create credentials DB (only once)
-# create_db(
-#   credentials_data = credentials,
-#   sqlite_path = "credentials.sqlite",
-#   # passphrase = key_get("xxh")
-# )
+create_db(
+  credentials_data = credentials,
+  sqlite_path = "credentials.sqlite",
+  # passphrase =passphrase
+)
 
 set_labels(
   language = "en",
